1125e5e563bd8de09ec04fcaf2711eb8008531da
Irandilson2020 <irandilson@alu.ufc.br>
2020-11-27 09:43:48 -0300

    Meu primeiro exemplo POO

---
+++ Projeto 01 Inicio/disciplina_POO/src/disciplina_POO/HelloWorld.java
@@ -0,0 +1,12 @@
+package disciplina_POO;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		 System.out.println("Hello World! Bem vindo a disciplina de POO!!");
+		 System.out.println("Sistemas de Informação - UFC Quixadá");
+    
+
+	}
+
+}
